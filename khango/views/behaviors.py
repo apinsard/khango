7370a045d59dee798a14b8e6747595fd158c503b
Antoine Pinsard <antoine.pinsard@gmail.com>
2016-03-08 19:56:11 +0100

    Started ContentTypeMixin

--- khango/views/behaviors.py
+++ khango/views/behaviors.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+import logging
+import re
+
 from django.core.exceptions import FieldDoesNotExist
+from django.http import HttpResponse, JsonResponse
+
+logger = logging.getLogger('django.request')
 
 
 class ModelMixin:
@@ -118,3 +124,103 @@ class ModelMixin:
     def get_prefetch_related(cls):
         cls._parse_fields()
         return cls.__prefetch_related
+
+
+class ContentTypeMixin:
+    """Return a suitable response according to the "Accept" request header."""
+
+    content_types = [
+        'text/html', 'application/json', 'application/xml',
+    ]
+
+    def dispatch(self, request, *args, **kwargs):
+        """Check if we can answer the request with an acceptable content-type.
+        If not, return HTTP 406 "Not Acceptable".
+        """
+        self.content_type = self.get_content_type()
+        if self.content_type is None:
+            return self.http_not_acceptable(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
+
+    def render_to_response(self, context, **response_kwargs):
+        """Select the response method according to the requested content type.
+        May raise NotImplementedError if no method matched.
+        """
+        response_kwargs['content_type'] = self.content_type
+        if self.content_type == 'text/html':
+            return self.render_html(context, **response_kwargs)
+        elif self.content_type == 'application/json':
+            return self.render_json(context, **response_kwargs)
+        elif self.content_type == 'application/xml':
+            return self.render_xml(context, **response_kwargs)
+        else:
+            raise NotImplementedError(
+                ("Render method for content type "
+                 "\"{}\" does not exist.").format(self.content_type)
+            )
+
+    def render_html(self, context, **response_kwargs):
+        """Return an HTML response."""
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
+
+    def render_json(self, context, **response_kwargs):
+        """Return context as JSON response."""
+        data = context.flatten()
+        del data['False'], data['True'], data['None']
+        return JsonResponse(data, **response_kwargs)
+
+    def render_xml(self, context, **response_kwargs):
+        """Return an XML response."""
+        raise NotImplementedError
+
+    def http_not_acceptable(self, request, *args, **kwargs):
+        logger.warning("Not Acceptable (%s): %s",
+                       request.META.get('HTTP_ACCEPT'), request.path,
+                       extra={
+                           'status_code': 406,
+                           'request': request,
+                       })
+        header = 'Accept'
+        values = ','.join(self.get_available_content_types())
+        return HttpResponse('{}: {}'.format(header, values), status_code=406,
+                            content_type='text/plain')
+
+    def get_content_type(self):
+        """Return the first content type accepted by the client and that we
+        know how to respond as well.
+        Return None if we are not able to respond with an acceptable content
+        type.
+        """
+        content_types = self.content_types
+        accepted = self.get_accepted_content_types()
+        for content_type in accepted:
+            if content_type in content_types:
+                return content_type
+        if '*/*' in accepted:
+            return content_types[0]
+        return None
+
+    def get_accepted_content_types(self):
+        """Return the list of content types accepted by the client for the
+        current request.
+        """
+        def qualify(x):
+            parts = x.split(';', 1)
+            if len(parts) == 2:
+                match = re.match(r'(^|;)q=(0(\.\d{,3})?|1(\.0{,3})?)(;|$)',
+                                 parts[1])
+                if match:
+                    return parts[0], float(match.group(2))
+            return parts[0], 1
+
+        http_accept = self.request.META.get('HTTP_ACCEPT', '*/*')
+        raw_content_types = http_accept.split(',')
+        qualified_content_types = map(qualify, raw_content_types)
+        return (x[0] for x in sorted(qualified_content_types,
+                                     key=lambda x: x[1], reverse=True))
