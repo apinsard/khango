4962a8b9ade5cf510ca63999f50dbe77503ea5b9
Antoine Pinsard <antoine.pinsard@gmail.com>
2016-03-08 20:37:37 +0100

    Setup ContentTypeMixin test cases

---
+++ khango/views/tests/test_content_type_mixin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+from ..behaviors import ContentMixin
+
+
+class GetContentTypeTestCase(TestCase):
+    pass
+
+
+class GetAcceptedContentTypes(TestCase):
+    pass
